b2bb6616cb552c1df7cf0d62f8ecfba45e379d26
Maria Baykova <mnbaikova@gmail.com>
2022-03-23 23:02:54 +0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+function addition() {
+    let firstNum = prompt( '1st number is ', "0");
+    let secondNum = prompt( '2nd number is ', "0");
+    alert( firstNum + " + " + secondNum + " = " + ( Number(firstNum) + Number(secondNum) ));
+  }
+  function multiplication() {
+    let firstNum = prompt( '1st number is ', "0");
+    let secondNum = prompt( '2nd number is ', "0");
+    alert( firstNum + " * " + secondNum + " = " + ( Number(firstNum) * Number(secondNum) ));
+  }
+  
+  function division() {
+    let firstNum = prompt( '1st number is ', "0");
+    let secondNum = prompt( '2nd number is ', "0");
+    alert( firstNum + " / " + secondNum + " = " + ( Number(firstNum) / Number(secondNum) ));
+  }
+  
+  function subtraction() {
+    let firstNum = prompt( '1st number is ', "0");
+    let secondNum = prompt( '2nd number is ', "0");
+    alert( firstNum + " - " + secondNum + " = " + ( Number(firstNum) - Number(secondNum) ));
+  }
+  
